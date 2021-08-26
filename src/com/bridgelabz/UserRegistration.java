c4d5c92d9c5b214ba3df1b92c11de72783533982
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-26 23:41:35 +0530

    Added: Enter a valid mobile number

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -11,15 +11,16 @@ public class UserRegistration {
     private String firstName = "^[A-Z]([a-z]{2,15})*";
     private String lastName = "^[A-Z]([a-z]{2,15})*";
     private String eMail = "^[a-z0-9]{3,}(.[0-9a-z]+)*@[a-z]{2,15}.(com|bl)(co.in)*$";
+    private String phoneNo ="^[91]+[ ]{0,1}+[1-9][0-9]{9}$";
     Scanner sn = new Scanner(System.in);
 
     public UserRegistration() {
         pattern = pattern.compile(firstName);
         pattern = pattern.compile(lastName);
         pattern =pattern.compile(eMail);
-
-
+        pattern = pattern.compile(phoneNo);
     }
+
     public boolean validateFirstName(String firstName) {
         matcher = pattern.matcher(firstName);
         return matcher.matches();
@@ -36,12 +37,20 @@ public class UserRegistration {
         return matcher.matches();
     }
 
+    public boolean validatePhoneNo(String phoneNo)
+    {
+        matcher = pattern.matcher(phoneNo);
+        return matcher.matches();
+    }
+
     public static void main(String[] args)
      {
-           UserRegistration userRegistration = new UserRegistration();
+         UserRegistration userRegistration = new UserRegistration();
          System.out.println(userRegistration.validateFirstName("Nam"));
          System.out.println(userRegistration.validateLastName("She"));
          System.out.println(userRegistration.validateEMail("nam.xyz@yahoo.com"));
+         System.out.println(userRegistration.validatePhoneNo("91 9587823545"));
+
 
        }
 }
