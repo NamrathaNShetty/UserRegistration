6b989331b9c56cfff648f1d3d28e63cc02f81462
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-26 21:11:24 +0530

    Added: Enter a first name with minimum 3 characters

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,26 @@
 package com.bridgelabz;
 
-public class UserRegistration {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration problem");
+public class UserRegistration {
+    private Pattern pattern;
+    private Matcher matcher;
+    private String firstName = "^[A-Z]([a-z]{2,15})*";
+    Scanner sn = new Scanner(System.in);
+
+    public UserRegistration() {
+        pattern = pattern.compile(firstName);
+
     }
+    public boolean validateFirstName(String firstName) {
+        matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+    public static void main(String[] args)
+     {
+           UserRegistration userRegistration = new UserRegistration();
+         System.out.println(userRegistration.validateFirstName("Nam"));
+       }
 }
