85d23f97b6c15b96d69d4a3e1dceabbc2040adc8
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-26 23:37:27 +0530

    Added: Enter a valid email address

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -10,11 +10,13 @@ public class UserRegistration {
     private Matcher matcher;
     private String firstName = "^[A-Z]([a-z]{2,15})*";
     private String lastName = "^[A-Z]([a-z]{2,15})*";
+    private String eMail = "^[a-z0-9]{3,}(.[0-9a-z]+)*@[a-z]{2,15}.(com|bl)(co.in)*$";
     Scanner sn = new Scanner(System.in);
 
     public UserRegistration() {
         pattern = pattern.compile(firstName);
         pattern = pattern.compile(lastName);
+        pattern =pattern.compile(eMail);
 
 
     }
@@ -28,10 +30,18 @@ public class UserRegistration {
         return matcher.matches();
     }
 
+    public boolean validateEMail(String eMail)
+    {
+        matcher = pattern.matcher(eMail);
+        return matcher.matches();
+    }
+
     public static void main(String[] args)
      {
            UserRegistration userRegistration = new UserRegistration();
          System.out.println(userRegistration.validateFirstName("Nam"));
          System.out.println(userRegistration.validateLastName("She"));
+         System.out.println(userRegistration.validateEMail("nam.xyz@yahoo.com"));
+
        }
 }
