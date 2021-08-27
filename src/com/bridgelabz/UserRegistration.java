d27cd5c532daceb295a5161a1e3eb37368fdb202
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-27 10:13:32 +0530

    Added: Validate all emails given in the list

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -5,33 +5,16 @@ import java.util.regex.Pattern;
 
 
     public class UserRegistration {
-    //variables
+        //variables
 
-    private String firstName = "^[A-Z]([a-z]{2,15})*";
-    private String lastName = "^[A-Z]([a-z]{2,15})*";
-    private String eMail = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
-    private String phoneNo ="^[91]+[ ]{0,1}+[1-9][0-9]{9}$";
-    private String password = "^[0-9A-Za-z]*[!@#$%&][A-Za-z0-9]*$";
+        private String firstName = "^[A-Z]([a-z]{2,15})*";
+        private String lastName = "^[A-Z]([a-z]{2,15})*";
+        private String eMail = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
+        private String phoneNo = "^[91]+[ ]{0,1}+[1-9][0-9]{9}$";
+        private String password = "^[0-9A-Za-z]*[!@#$%&][A-Za-z0-9]*$";
 
-
-        public  boolean validateFirstName(String firstName)
-        {
-            return Pattern.matches(this.firstName,firstName);
-        }
-        public  boolean validateLastName(String lastName)
-        {
-            return Pattern.matches(this.lastName,lastName);
-        }
-        public boolean validateEMail(String eMail)
-        {
-            return Pattern.matches(this.eMail,eMail);
-        }
-        public boolean validatePhoneNo(String phoneNo)
-        {
-            return Pattern.matches(this.phoneNo,phoneNo);
-        }
-        public boolean validatePassword(String password)
-        {
-            return Pattern.matches(this.password,password);
+        /*Validate all the emails*/
+        public boolean validateEMail(String eMail) {
+            return Pattern.matches(this.eMail, eMail);
         }
     }
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -11,41 +11,17 @@ public class UserRegistrationMain
         boolean check = true;
         while (check)
         {
-            System.out.println("1.First Name  2.Last Name  3.EMail  4.PhoneNo 5.Password  6.Quit");
+            System.out.println("1.EMail 2.Quit");
             int option = scanner.nextInt();
             switch (option)
             {
                 case 1:
-                    System.out.println("Enter First Name");
-                    String firstName = scanner.next();
-                    System.out.println(userRegistration.validateFirstName(firstName));
-                    break;
-
-                case 2:
-                    System.out.println("Enter Last Name");
-                    String lastName = scanner.next();
-                    System.out.println(userRegistration.validateLastName(lastName));
-                    break;
-
-                case 3:
                     System.out.println("Enter Valid Email");
                     String eMail = scanner.next();
                     System.out.println(userRegistration.validateEMail(eMail));
                     break;
 
-                case 4:
-                    System.out.println("Enter Phone No");
-                    String phoneNo = scanner.next();
-                    System.out.println(userRegistration.validatePhoneNo(phoneNo));
-                    break;
-
-                case 5:
-                    System.out.println("Enter Password");
-                    String password = scanner.next();
-                    System.out.println(userRegistration.validatePassword(password));
-                    break;
-
-                case 6:
+                case 2:
                     check = false;
 
             }
