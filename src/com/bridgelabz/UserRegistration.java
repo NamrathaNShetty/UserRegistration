a5c5ffd3a26afc2dcb6f333bfe839be8c2bbef89
NamrathaNShetty <namratanshetty@gmail.com>
2021-09-06 09:14:24 +0530

    Added: Refactored using Lambda Function

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,161 +1,96 @@
 package com.bridgelabz;
 
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
-
+interface Validation{
+    boolean validate();
+}
     public class UserRegistration {
     //variables
+    static Scanner sc = new Scanner(System.in);
+        static UserRegistration userReg = new UserRegistration();
 
     private String firstName = "^[A-Z]([a-z]{2,15})*";
-    private String lastName = "^[A-Z]([a-z]{2,15})*";
     private String eMail = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
     private String phoneNo ="^[91]+[ ]{0,1}+[1-9][0-9]{9}$";
     private String password = "^[0-9A-Za-z]*[!@#$%&][A-Za-z0-9]*$";
 
-        //public class UserRegistration {
+        public static void main(String[] args) {
 
-            public boolean firstName(String fname) {
-                boolean answer = Pattern.matches("^[A-Z]([a-z]{2,15})*",fname);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid first Name.");
-                    }
-                }catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
+            Validation FirstName = () -> {System.out.println("Enter first name :: ");String firstName = sc.next();return userReg.Name(firstName);};
+            FirstName.validate();
 
-            public boolean lastName(String lname) throws InvalidEntryException {
-                boolean answer = Pattern.matches("^[A-Z]([a-z]{2,15})*", lname);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Last Name.");
-                    }
-                }catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
+            Validation LastName = () -> {System.out.println("Enter Last name :: ");String lastName = sc.next();return userReg.Name(lastName);};
+            LastName.validate();
 
-            public boolean Email(String email) throws InvalidEntryException {
-                boolean answer = Pattern.matches("[a-z]+([.][a-z]+)*@bl[.]co([.]in)*", email);
-                try {
-                    if (answer) {
-                        System.out.println("thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Email ID.");
-                    }
-                }catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
+            Validation Email = () -> {System.out.println("Enter Email ID :: ");String EmailId = sc.next();return userReg.Email(EmailId);};
+            Email.validate();
 
-            public boolean contactNumber(String ContactNumber) {
-                boolean answer = Pattern.matches("^[91]+[ ]{0,1}+[1-9][0-9]{9}$", ContactNumber);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Contact Number.");
-                    }
-                }
-                catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
+            Validation MobNum = () -> {System.out.println("Enter mobile number ::");String n = sc.next();return userReg.Number(n);};
+            MobNum.validate();
 
-            public boolean password1(String password) throws InvalidEntryException {
-                // rule1 = minimum 8 characters
-                boolean answer = Pattern.matches("[a-zA-Z]{8,}", password);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    }
-                    else {
-                        throw new InvalidEntryException("Please enter valid password.");
-                    }
-                }
-                catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
-            public boolean password2(String password) throws InvalidEntryException {
-                // rule1 = minimum 8 characters and 1 upper case
-                boolean answer = Pattern.matches("[A-Z][a-z]{8,}", password);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Password.");
-                    }
-                }
-                catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
-
-            public boolean password3(String password) throws InvalidEntryException {
-                // rule1 = minimum 8 characters and 1 upper case
-                boolean answer = Pattern.matches("([A-Z]+[a-z]+[0-9]+)", password);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Password.");
-                    }
-                }
-                catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
-
-            public boolean password4(String password) throws InvalidEntryException {
-                // rule1 = minimum 8 characters and 1 upper case
-                boolean answer = Pattern.matches("^[0-9A-Za-z]*[!@#$%&][A-Za-z0-9]*$", password);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Password.");
-                    }
-                }
-                catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
-
-            public boolean EmailValidation(String email) throws InvalidEntryException {
-                boolean answer = Pattern.matches("[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email);
-                try {
-                    if (answer) {
-                        System.out.println("Thank you");
-                        return true;
-                    } else {
-                        throw new InvalidEntryException("Please enter valid Last Name.");
-                    }
-                }catch (InvalidEntryException e ){
-                    System.out.println(e.getMessage());
-                }
-                return false;
-            }
-
+            Validation Password = () ->{System.out.println("Please Enter password :: ");String password = sc.next();return userReg.password(password);};
+            Password.validate();
         }
+
+        public boolean Name(String N) {
+            boolean answer = Pattern.matches(firstName, N);
+            try {
+                if (answer) {
+                    System.out.println("Thank you");
+                    return true;
+                } else {
+                    throw new InvalidEntryException("Please enter valid First name or Last name.");
+                }
+            }catch (InvalidEntryException e ){
+                System.out.println(e.getMessage());
+            }
+            return false;
+        }
+
+        public boolean Email(String ID) {
+            boolean answer = Pattern.matches(eMail, ID);
+            try {
+                if (answer) {
+                    System.out.println("Thank you");
+                    return true;
+                } else {
+                    throw new InvalidEntryException("Please enter valid Email ID.");
+                }
+            }catch (InvalidEntryException e ){
+                System.out.println(e.getMessage());
+            }
+            return false;
+        }
+
+        public boolean Number(String num) {
+            boolean answer = Pattern.matches(phoneNo, num);
+            try {
+                if (answer) {
+                    System.out.println("Thank you");
+                    return true;
+                } else {
+                    throw new InvalidEntryException("Please enter valid Phone Number.");
+                }
+            }catch (InvalidEntryException e ){
+                System.out.println(e.getMessage());
+            }
+            return false;
+        }
+
+        public boolean password(String p) {
+            boolean answer = Pattern.matches(password, p);
+            try {
+                if (answer) {
+                    return true;
+                } else {
+                    throw new InvalidEntryException("Please enter valid password.");
+                }
+            }catch (InvalidEntryException e ){
+                System.out.println(e.getMessage());
+            }
+            return false;
+        }
+    }
--- src/com/bridgelabz/UserRegistrationTest.java
+++ src/com/bridgelabz/UserRegistrationTest.java
@@ -1,103 +1,61 @@
 package com.bridgelabz;
 
-import org.junit.Assert;
 import org.junit.Test;
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UserRegistrationTest {
-    UserRegistration valid = new UserRegistration();
 
-    @Test
-    public void givenFirstName_WhenValid_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.firstName("Namratha");
-        Assert.assertTrue(result);
-    }
+    UserRegistration validator = new UserRegistration();
 
+    // Test cases to validate First Name and Last Name.
     @Test
-    public void givenFirstName_WhenShort_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.firstName("Na");
-        Assert.assertFalse(result);
-    }
-    @Test
-    public void givenLastName_WhenValid_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.lastName("Shetty");
-        Assert.assertTrue(result);
+    public void givenFirstName_WhenValid_ShouldReturnTrue() {
+        String FirstName = "Namratha";
+        assertTrue(validator.Name(FirstName));
     }
 
     @Test
-    public void givenLastName_WhenShort_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.lastName("Sh");
-        Assert.assertFalse(result);
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        String FirstName = "Na";
+        assertFalse(validator.Name(FirstName));
+    }
+
+    // Test cases to validate Email
+    @Test
+    public void givenEmail_WhenValid_ShouldReturnTrue() {
+        String eMail = "mfjkd@bl.co";
+        assertTrue(validator.Email(eMail));
     }
 
     @Test
-    public void givenEmail_WhenValid_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.Email("abc.xyz@bl.co.in");
-        Assert.assertTrue(result);
+    public void givenEmail_WhenShort_ShouldReturnFalse() {
+        String eMail = "namra79*@bl.co";
+        assertFalse(validator.Email(eMail));
+    }
+    // Test cases to validate Phone number
+    @Test
+    public void givenNumber_WhenValid_ShouldReturnTrue() {
+        String phoneNo= "91 1234567890";
+        assertFalse(validator.Email(phoneNo));
     }
 
     @Test
-    public void givenEmail_WhenNotValid_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.Email("abc.xyz@bl.in");
-        Assert.assertFalse(result);
+    public void givenNumber_WhenNotValid_ShouldReturnFalse() {
+        String phoneNo = "1234567890";
+        assertFalse(validator.Email(phoneNo));
     }
+
+    //  // Test cases to validate Password
     @Test
-    public void givenContactNumber_WhenValid_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.contactNumber("91 9923456789");
-        Assert.assertTrue(result);
+    public void givenPassword_WhenValid_ShouldReturnTrue() {
+        String password = "Abcd1234$";
+        assertTrue(validator.password(password));
     }
 
     @Test
-    public void givenContactNumber_WhenNotValid_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.contactNumber("1234567890");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenPassword1_WhenValid_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.password1("abcdbaae");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenPassword1_WhenShort_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.password1("abcdb");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenPassword2_WhenOneUpp_Case_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.password2("Qwertyuiop");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenPassword2_WhenNoUpp_Case_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.password2("qwertyuiop");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenPassword3_WhenOne_Uppcase_One_num_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.password3("awertyuek");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenPassword4_WhenOneUpCase_OneNum_OneSpecial_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.password4("Qwertyue122$");
-        Assert.assertTrue(result);
-    }
-
-    @Test
-    public void givenPassword4_WhenNo_OneUpCase_OneNum_OneSpecial_ShouldReturnFalse() throws InvalidEntryException {
-        boolean result = valid.password4("Qwertyue12");
-        Assert.assertFalse(result);
-    }
-
-    @Test
-    public void givenEmailValidation_WhenValid_ShouldReturnTrue() throws InvalidEntryException {
-        boolean result = valid.EmailValidation("abc.xyz@bl.co.in");
-        Assert.assertTrue(result);
+    public void givenPassword_WhenNotValid_ShouldReturnFalse() {
+        String password = "Abcdefgh";
+        assertFalse(validator.password(password));
     }
 }
